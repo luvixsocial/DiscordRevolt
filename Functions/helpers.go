e05466f9b1f21e6ce5b7082a1290c63765df89d5
Luvix Social <selectdev@purrquinox.com>
2025-05-10 13:54:04 +0000

    - Added embeds - Added edit_message capability

--- Functions/helpers.go
+++ Functions/helpers.go
@@ -20,39 +20,118 @@ func convertOptionsToMap(options []*discordgo.ApplicationCommandInteractionDataO
 }
 
 // Helper function to respond to any event.
-func Respond(e types.Event, content string) error {
+func Respond(e types.Event, content string, embed *types.Embed, edit *string) (interface{}, error) {
 	switch e.Platform {
 	case "Discord":
+		session := e.Session.(*discordgo.Session)
+
 		switch ctx := e.Context.(type) {
 		case *discordgo.MessageCreate:
-			session := e.Session.(*discordgo.Session)
-			_, err := session.ChannelMessageSendReply(ctx.ChannelID, content, &discordgo.MessageReference{
-				MessageID: ctx.ID,
-				ChannelID: ctx.ChannelID,
-				GuildID:   ctx.GuildID,
-			})
-			return err
+			if edit != nil {
+				// Edit message
+				editMsg := &discordgo.MessageEdit{
+					ID:      *edit,
+					Channel: ctx.ChannelID,
+					Content: &content,
+				}
+				if embed != nil {
+					embeds := []*discordgo.MessageEmbed{{
+						Title:       embed.Title,
+						Description: embed.Description,
+						Color:       embed.Color,
+					}}
+					editMsg.Embeds = &embeds
+				}
+				return session.ChannelMessageEditComplex(editMsg)
+			}
+
+			// Send message
+			msg := &discordgo.MessageSend{
+				Content: content,
+				Reference: &discordgo.MessageReference{
+					MessageID: ctx.ID,
+					ChannelID: ctx.ChannelID,
+					GuildID:   ctx.GuildID,
+				},
+			}
+			if embed != nil {
+				msg.Embeds = []*discordgo.MessageEmbed{{
+					Title:       embed.Title,
+					Description: embed.Description,
+					Color:       embed.Color,
+				}}
+			}
+			return session.ChannelMessageSendComplex(ctx.ChannelID, msg)
 
 		case *discordgo.InteractionCreate:
-			session := e.Session.(*discordgo.Session)
+			if edit != nil {
+				// Edit interaction response
+				editData := &discordgo.WebhookEdit{
+					Content: &content,
+				}
+				if embed != nil {
+					embeds := []*discordgo.MessageEmbed{{
+						Title:       embed.Title,
+						Description: embed.Description,
+						Color:       embed.Color,
+					}}
+					editData.Embeds = &embeds
+				}
+				resp, err := session.InteractionResponseEdit(ctx.Interaction, editData)
+				return resp, err
+			}
+
+			// Respond to interaction
+			data := &discordgo.InteractionResponseData{
+				Content: content,
+			}
+			if embed != nil {
+				data.Embeds = []*discordgo.MessageEmbed{{
+					Title:       embed.Title,
+					Description: embed.Description,
+					Color:       embed.Color,
+				}}
+			}
 			err := session.InteractionRespond(ctx.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Content: content,
-				},
+				Data: data,
 			})
-			return err
+			return nil, err
 		}
 
 	case "Revolt":
-		if ctx, ok := e.Context.(*revoltgo.EventMessage); ok {
-			session := e.Session.(*revoltgo.Session)
-			_, err := session.ChannelMessageSend(ctx.Channel, revoltgo.MessageSend{
-				Content: content,
-			})
-			return err
+		session := e.Session.(*revoltgo.Session)
+		ctx, ok := e.Context.(*revoltgo.EventMessage)
+		if !ok {
+			break
 		}
+
+		if edit != nil {
+			editMsg := revoltgo.MessageEditData{
+				Content: content,
+			}
+			if embed != nil {
+				editMsg.Embeds = []*revoltgo.MessageEmbed{{
+					Title:       embed.Title,
+					Description: embed.Description,
+					Colour:      fmt.Sprintf("#%06X", embed.Color),
+				}}
+			}
+			return session.ChannelMessageEdit(ctx.Channel, *edit, editMsg)
+		}
+
+		sendMsg := revoltgo.MessageSend{
+			Content: content,
+		}
+		if embed != nil {
+			sendMsg.Embeds = []*revoltgo.MessageEmbed{{
+				Title:       embed.Title,
+				Description: embed.Description,
+				Colour:      fmt.Sprintf("#%06X", embed.Color),
+			}}
+		}
+		return session.ChannelMessageSend(ctx.Channel, sendMsg)
 	}
 
-	return fmt.Errorf("unsupported platform or context")
+	return nil, fmt.Errorf("unsupported platform or context")
 }
