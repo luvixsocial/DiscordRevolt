9e064080ccc057c22754bda1b7e13da43bba28dc
Luvix Social <selectdev@purrquinox.com>
2025-05-10 13:53:46 +0000

    Improved configuration structure

--- Functions/config.go
+++ Functions/config.go
@@ -1,16 +1,19 @@
 package functions
 
 import (
+	"libdozina/types"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/sentinelb51/revoltgo"
 )
 
 // Configure both Discord and Revolt clients.
-func Config(discord string, revolt string) {
+func Config(config *types.Config) {
 	var err error
-	Revolt = revoltgo.New(revolt)
-	Discord, err = discordgo.New("Bot " + discord)
+	Discord, err = discordgo.New("Bot " + config.Discord.Token)
 	if err != nil {
 		panic(err)
 	}
+
+	Revolt = revoltgo.New(config.Revolt.Token)
 }
